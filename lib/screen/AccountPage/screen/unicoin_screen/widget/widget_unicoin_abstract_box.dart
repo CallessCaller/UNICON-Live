51b15fe774e8eb02c364a829d72d0505d603347b
CallessCaller <pht0639@gmail.com>
2020-11-16 19:06:17 +0900

    Park

--- lib/screen/AccountPage/screen/unicoin_screen/widget/widget_unicoin_abstract_box.dart
+++ lib/screen/AccountPage/screen/unicoin_screen/widget/widget_unicoin_abstract_box.dart
@@ -1,10 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:testing_layout/components/constant.dart';
 import 'package:testing_layout/components/uni_icon_icons.dart';
 import 'package:testing_layout/model/users.dart';
-import 'package:testing_layout/screen/AccountPage/screen/unicoin_screen/widget/payModule.dart';
 
 class UnicoinAbstractBox extends StatefulWidget {
   final UserDB userDB;
@@ -15,9 +13,6 @@ class UnicoinAbstractBox extends StatefulWidget {
 }
 
 class _UnicoinAbstractBoxState extends State<UnicoinAbstractBox> {
-  TextEditingController coinAmount = new TextEditingController();
-  FocusNode _focusNode = new FocusNode();
-
   Widget _fetchData(BuildContext context) {
     return StreamBuilder<DocumentSnapshot>(
       stream: FirebaseFirestore.instance
@@ -39,112 +34,31 @@ class _UnicoinAbstractBoxState extends State<UnicoinAbstractBox> {
   Widget _buildBody(BuildContext context, DocumentSnapshot snapshot) {
     UserDB _userDB = UserDB.fromSnapshot(snapshot);
     return Container(
+      alignment: Alignment.center,
       padding: EdgeInsets.fromLTRB(25, 10, 25, 10),
-      height: 100,
+      height: 50,
+      width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.white, width: 1),
         borderRadius: BorderRadius.circular(widgetRadius),
       ),
-      child: Column(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Icon(
-                    UniIcon.unicoin,
-                    color: _userDB.points != 0 ? appKeyColor : Colors.grey,
-                  ),
-                  SizedBox(width: 5),
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      _userDB.points.toString(),
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: subtitleFontSize,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                height: 35,
-                width: MediaQuery.of(context).size.width * 0.3,
-                child: TextField(
-                  focusNode: _focusNode,
-                  controller: coinAmount,
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                      hintText: '충전 코인 입력',
-                      hintStyle:
-                          TextStyle(fontSize: textFontSize, color: Colors.grey),
-                      contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                      focusedBorder: OutlineInputBorder(
-                          gapPadding: 0,
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(7)),
-                      border: OutlineInputBorder(
-                          gapPadding: 0,
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(7))),
-                ),
-              ),
-              FlatButton(
-                minWidth: 44,
-                height: 30,
-                color: appKeyColor,
-                disabledColor: Colors.grey,
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(widgetRadius),
-                ),
-                onPressed: coinAmount.text == ''
-                    ? null
-                    : int.parse(coinAmount.text) < 500
-                        ? null
-                        : () {
-                            int stringToInt = int.parse(coinAmount.text);
-                            if (stringToInt < 500) {
-                              Fluttertoast.showToast(
-                                msg: '500코인 이상 입력해주세요.',
-                                backgroundColor: Colors.black,
-                                fontSize: textFontSize,
-                                toastLength: Toast.LENGTH_SHORT,
-                                gravity: ToastGravity.BOTTOM,
-                              );
-                            } else {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => PayModule(
-                                        userDB: _userDB,
-                                        coinAmount: stringToInt,
-                                      )));
-                            }
-
-                            coinAmount.text = '';
-                            _focusNode.unfocus();
-                          },
-                child: Text(
-                  '충전',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: textFontSize,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ),
-            ],
+          Icon(
+            UniIcon.unicoin,
+            size: 30,
+            color: _userDB.points != 0 ? appKeyColor : Colors.grey,
           ),
-          Container(
-              alignment: Alignment.center,
-              width: MediaQuery.of(context).size.width * 0.7,
-              child: Text(coinAmount.text == ''
-                  ? ''
-                  : int.parse(coinAmount.text) < 500
-                      ? '500코인 이상부터 자유 충전 가능합니다.'
-                      : '${getPrice(coinAmount.text)}원')),
+          SizedBox(width: 5),
+          Text(
+            _userDB.points.toString(),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: subtitleFontSize + 2,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
         ],
       ),
     );
@@ -154,19 +68,4 @@ class _UnicoinAbstractBoxState extends State<UnicoinAbstractBox> {
   Widget build(BuildContext context) {
     return _fetchData(context);
   }
-}
-
-int getPrice(String coinAmount) {
-  int coinInt = int.parse(coinAmount);
-  int coinPrice = 0;
-  if (coinInt < 50) {
-    coinPrice = coinInt * 120;
-  } else if (coinInt < 100) {
-    coinPrice = coinInt * (120 * 0.98).round();
-  } else if (coinInt < 500) {
-    coinPrice = coinInt * (120 * 0.95).round();
-  } else {
-    coinPrice = coinInt * (120 * 0.97).round();
-  }
-  return (coinPrice);
 }
--- lib/screen/AccountPage/screen/unicoin_screen/widget/widget_unicoin_pricing_list.dart
+++ lib/screen/AccountPage/screen/unicoin_screen/widget/widget_unicoin_pricing_list.dart
@@ -1,10 +1,22 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:testing_layout/components/constant.dart';
 import 'package:testing_layout/components/uni_icon_icons.dart';
 import 'package:testing_layout/model/users.dart';
-import 'package:testing_layout/screen/AccountPage/screen/unicoin_screen/widget/payModule.dart';
-import 'package:testing_layout/screen/AccountPage/screen/unicoin_screen/widget/widget_unicoin_abstract_box.dart';
+import 'package:testing_layout/screen/AccountPage/screen/unicoin_screen/widget/consumable_store.dart';
+
+const bool _kAutoConsume = true;
+
+const String _kConsumableId = 'consumable';
+const List<String> _kProductIds = <String>[
+  _kConsumableId,
+  'upgrade',
+  'subscription'
+];
 
 class UnicoinPricingList extends StatefulWidget {
   final UserDB userDB;
@@ -15,6 +27,39 @@ class UnicoinPricingList extends StatefulWidget {
 }
 
 class _UnicoinPricingListState extends State<UnicoinPricingList> {
+  final InAppPurchaseConnection _connection = InAppPurchaseConnection.instance;
+  StreamSubscription<List<PurchaseDetails>> _subscription;
+  List<String> _notFoundIds = [];
+  List<ProductDetails> _products = [];
+  List<PurchaseDetails> _purchases = [];
+  List<String> _consumables = [];
+  bool _isAvailable = false;
+  bool _purchasePending = false;
+  bool _loading = true;
+  String _queryProductError;
+
+  @override
+  void initState() {
+    super.initState();
+    Stream purchaseUpdated =
+        InAppPurchaseConnection.instance.purchaseUpdatedStream;
+    _subscription = purchaseUpdated.listen((purchaseDetailsList) {
+      _listenToPurchaseUpdated(purchaseDetailsList);
+    }, onDone: () {
+      _subscription.cancel();
+    }, onError: (error) {
+      // handle error here.
+    });
+    initStoreInfo();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _subscription.cancel();
+    super.dispose();
+  }
+
   Widget _fetchData(BuildContext context) {
     return StreamBuilder<DocumentSnapshot>(
       stream: FirebaseFirestore.instance
@@ -35,6 +80,29 @@ class _UnicoinPricingListState extends State<UnicoinPricingList> {
 
   Widget _buildBody(BuildContext context, DocumentSnapshot snapshot) {
     UserDB _userDB = UserDB.fromSnapshot(snapshot);
+
+    List<Widget> stack = [];
+    if (_queryProductError == null) {
+      stack.add(
+        Column(
+          children: [
+            _buildProductList(_userDB),
+          ],
+        ),
+      );
+    } else {
+      stack.add(Center(
+        child: Text(_queryProductError),
+      ));
+    }
+    if (_purchasePending) {
+      stack.add(
+        Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
       decoration: BoxDecoration(
@@ -44,7 +112,7 @@ class _UnicoinPricingListState extends State<UnicoinPricingList> {
       child: Column(
         children: [
           Text(
-            '유니코인 가격표',
+            '유니코인 충전',
             style: TextStyle(
               fontSize: subtitleFontSize,
               fontWeight: FontWeight.w600,
@@ -53,11 +121,9 @@ class _UnicoinPricingListState extends State<UnicoinPricingList> {
           SizedBox(
             height: 20,
           ),
-          priceTag(_userDB, 10, getPrice('10').toString()),
-          priceTag(_userDB, 20, getPrice('20').toString()),
-          priceTag(_userDB, 50, getPrice('50').toString()),
-          priceTag(_userDB, 100, getPrice('100').toString()),
-          priceTag(_userDB, 500, getPrice('500').toString()),
+          Stack(
+            children: stack,
+          )
         ],
       ),
     );
@@ -68,21 +134,56 @@ class _UnicoinPricingListState extends State<UnicoinPricingList> {
     return _fetchData(context);
   }
 
-  Widget priceButton(UserDB userdB, int amount, String price) {
-    return FlatButton(
-      minWidth: 95,
-      height: 30,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7)),
-      color: appKeyColor,
-      onPressed: () {
-        Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => PayModule(
-                  userDB: userdB,
-                  coinAmount: amount,
-                )));
+  Widget _buildProductList(UserDB userDB) {
+    if (_loading) {
+      return Card(
+          child: (ListTile(
+              leading: CircularProgressIndicator(),
+              title: Text('Fetching products...'))));
+    }
+    if (!_isAvailable) {
+      return Card();
+    }
+
+    List<ListTile> productList = <ListTile>[];
+    if (_notFoundIds.isNotEmpty) {
+      productList.add(ListTile(
+          title: Text('[${_notFoundIds.join(", ")}] not found',
+              style: TextStyle(color: ThemeData.light().errorColor)),
+          subtitle: Text('error')));
+    }
+
+    productList.addAll(_products.map(
+      (ProductDetails productDetails) {
+        return priceTag(userDB, productDetails);
       },
+    ));
+
+    return Column(children: productList);
+  }
+
+  Widget priceButton(UserDB userdB, ProductDetails productDetails) {
+    return FlatButton(
+      onPressed: () {
+        PurchaseParam purchaseParam = PurchaseParam(
+            productDetails: productDetails,
+            applicationUserName: null,
+            sandboxTesting: true);
+        if (productDetails.id == _kConsumableId) {
+          _connection.buyConsumable(
+              purchaseParam: purchaseParam,
+              autoConsume: _kAutoConsume || Platform.isIOS);
+        } else {
+          _connection.buyNonConsumable(purchaseParam: purchaseParam);
+        }
+      },
+      color: appKeyColor,
+      minWidth: 110,
+      height: 30,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(widgetRadius)),
       child: Text(
-        price + '원',
+        productDetails.price + '원',
         textAlign: TextAlign.center,
         style: TextStyle(
           color: Colors.white,
@@ -93,18 +194,21 @@ class _UnicoinPricingListState extends State<UnicoinPricingList> {
     );
   }
 
-  Widget priceTag(UserDB userDB, int amount, String price) {
+  Widget priceTag(UserDB userDB, ProductDetails productDetails) {
     return Row(
       children: [
-        Icon(UniIcon.unicoin),
+        Icon(
+          UniIcon.unicoin,
+          size: 25,
+        ),
         SizedBox(
           width: 10,
         ),
         Container(
-          width: 30,
+          width: 40,
           alignment: Alignment.center,
           child: Text(
-            amount.toString(),
+            productDetails.title,
             textAlign: TextAlign.center,
             style: TextStyle(
               color: Colors.white,
@@ -117,8 +221,145 @@ class _UnicoinPricingListState extends State<UnicoinPricingList> {
             child: Container(
           child: Text(''),
         )),
-        priceButton(userDB, amount, price),
+        priceButton(userDB, productDetails),
       ],
     );
+  }
+
+  Future<void> initStoreInfo() async {
+    final bool isAvailable = await _connection.isAvailable();
+    if (!isAvailable) {
+      setState(() {
+        _isAvailable = isAvailable;
+        _products = [];
+        _purchases = [];
+        _notFoundIds = [];
+        _consumables = [];
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    ProductDetailsResponse productDetailResponse =
+        await _connection.queryProductDetails(_kProductIds.toSet());
+    if (productDetailResponse.error != null) {
+      setState(() {
+        _queryProductError = productDetailResponse.error.message;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+        _purchases = [];
+        _notFoundIds = productDetailResponse.notFoundIDs;
+        _consumables = [];
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    if (productDetailResponse.productDetails.isEmpty) {
+      setState(() {
+        _queryProductError = null;
+        _isAvailable = isAvailable;
+        _products = productDetailResponse.productDetails;
+        _purchases = [];
+        _notFoundIds = productDetailResponse.notFoundIDs;
+        _consumables = [];
+        _purchasePending = false;
+        _loading = false;
+      });
+      return;
+    }
+
+    final QueryPurchaseDetailsResponse purchaseResponse =
+        await _connection.queryPastPurchases();
+    if (purchaseResponse.error != null) {
+      // handle query past purchase error..
+    }
+    final List<PurchaseDetails> verifiedPurchases = [];
+    for (PurchaseDetails purchase in purchaseResponse.pastPurchases) {
+      if (await _verifyPurchase(purchase)) {
+        verifiedPurchases.add(purchase);
+      }
+    }
+    List<String> consumables = await ConsumableStore.load();
+    setState(() {
+      _isAvailable = isAvailable;
+      _products = productDetailResponse.productDetails;
+      _purchases = verifiedPurchases;
+      _notFoundIds = productDetailResponse.notFoundIDs;
+      _consumables = consumables;
+      _purchasePending = false;
+      _loading = false;
+    });
+  }
+
+  void _listenToPurchaseUpdated(List<PurchaseDetails> purchaseDetailsList) {
+    purchaseDetailsList.forEach((PurchaseDetails purchaseDetails) async {
+      if (purchaseDetails.status == PurchaseStatus.pending) {
+        showPendingUI();
+      } else {
+        if (purchaseDetails.status == PurchaseStatus.error) {
+          handleError(purchaseDetails.error);
+        } else if (purchaseDetails.status == PurchaseStatus.purchased) {
+          bool valid = await _verifyPurchase(purchaseDetails);
+          if (valid) {
+            deliverProduct(purchaseDetails);
+          } else {
+            _handleInvalidPurchase(purchaseDetails);
+            return;
+          }
+        }
+        if (Platform.isAndroid) {
+          if (!_kAutoConsume && purchaseDetails.productID == _kConsumableId) {
+            await InAppPurchaseConnection.instance
+                .consumePurchase(purchaseDetails);
+          }
+        }
+        if (purchaseDetails.pendingCompletePurchase) {
+          await InAppPurchaseConnection.instance
+              .completePurchase(purchaseDetails);
+        }
+      }
+    });
+  }
+
+  void deliverProduct(PurchaseDetails purchaseDetails) async {
+    // IMPORTANT!! Always verify a purchase purchase details before delivering the product.
+    if (purchaseDetails.productID == _kConsumableId) {
+      await ConsumableStore.save(purchaseDetails.purchaseID);
+      List<String> consumables = await ConsumableStore.load();
+      setState(() {
+        _purchasePending = false;
+        _consumables = consumables;
+      });
+    } else {
+      setState(() {
+        _purchases.add(purchaseDetails);
+        _purchasePending = false;
+      });
+    }
+  }
+
+  void _handleInvalidPurchase(PurchaseDetails purchaseDetails) {
+    // handle invalid purchase here if  _verifyPurchase` failed.
+  }
+
+  Future<bool> _verifyPurchase(PurchaseDetails purchaseDetails) {
+    // IMPORTANT!! Always verify a purchase before delivering the product.
+    // For the purpose of an example, we directly return true.
+    return Future<bool>.value(true);
+  }
+
+  void showPendingUI() {
+    setState(() {
+      _purchasePending = true;
+    });
+  }
+
+  void handleError(IAPError error) {
+    setState(() {
+      _purchasePending = false;
+    });
   }
 }
